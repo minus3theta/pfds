0f86b420bf27715759be088cb03ee5c01ef787a6
minus3theta <yone_j_synthesis@yahoo.co.jp>
2017-06-24 01:07:55 +0900

    Implement up to Exercise 2.5 (a)

--- Bst.hs
+++ Bst.hs
@@ -32,3 +32,33 @@ insert x (T l y r @ s) =
     T l y (insert x r)
   else
     s
+
+insert2 :: Ord e => e -> Tree e -> Tree e
+insert2 x t = maybe t id $ go t Just
+  where go E cont = cont $ T E x E
+        go (T l y r) cont =
+          if x < y then
+            go l (\u -> cont $ T u y r)
+          else if x > y then
+            go r (\u -> cont $ T l y u)
+          else
+            Nothing
+
+insert3 :: Ord e => e -> Tree e -> Tree e
+insert3 x t = maybe t id $ go t Nothing Just
+  where go E Nothing cont = cont $ T E x E
+        go E (Just y) cont =
+          if x == y then
+            Nothing
+          else
+            cont $ T E x E
+        go (T l y r) can cont =
+          if x < y then
+            go l can (\u -> cont $ T u y r)
+          else
+            go r (Just y) (\u -> cont $ T l y u)
+
+complete :: e -> Int -> Tree e
+complete _ 0 = E
+complete x n = let t = complete x $ n-1 in
+                 T t x t
