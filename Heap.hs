bddaa4ad98ed41e3e97c47f7fa761e5045f5aeaf
minus3theta <yone_j_synthesis@yahoo.co.jp>
2017-06-27 23:12:17 +0900

    Update Heap.hs Implement Ex. 3.3

--- Heap.hs
+++ Heap.hs
@@ -25,6 +25,14 @@ merge h1@(T _ x1 l1 r1) h2@(T _ x2 l2 r2) =
 insert :: Ord e => e -> Heap e -> Heap e
 insert x h = merge (T 1 x E E) h
 
+findMin :: Heap e -> Maybe e
+findMin E = Nothing
+findMin (T _ x _ _) = Just x
+
+deleteMin :: Ord e => Heap e -> Maybe (Heap e)
+deleteMin E = Nothing
+deleteMin (T _ _ l r) = Just $ merge l r
+
 -- Ex. 3.2
 insert' :: Ord e => e -> Heap e -> Heap e
 insert' x E = T 1 x E E
@@ -33,3 +41,12 @@ insert' x h@(T _ y l r) =
     makeT x E h
   else
     makeT y l $ insert' x r
+
+-- Ex. 3.3
+fromList :: Ord e => [e] -> Heap e
+fromList l = go $ map (\x -> T 1 x E E) l
+  where go [] = E
+        go [h] = h
+        go hs = go $ go' hs
+        go' (a:b:hs) = merge a b : go' hs
+        go' hs = hs
